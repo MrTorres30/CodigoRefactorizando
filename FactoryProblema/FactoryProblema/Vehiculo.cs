a77360d9795a575d8223077ae2f5e31b6cf0f014
MrTorres30 <torresandelizw@gmail.com>
2026-06-14 18:23:43 -0400

    Cree la clase VehiculoFactory

---
+++ FactoryProblema/FactoryProblema/Vehiculo.cs
@@ -0,0 +1,22 @@
+namespace FactoryPtoblema
+{
+    
+    public abstract class Vehiculo
+    {
+        
+        private string marca;
+        public string Marca {get;}
+
+        private double precio;
+        public double Precio {get;}
+    }
+
+
+
+
+
+
+
+
+
+}
